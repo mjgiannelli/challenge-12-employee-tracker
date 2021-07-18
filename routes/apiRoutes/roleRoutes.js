097754e95deae5460bdf33af8a55f05e0ed0706a
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-07-18 13:21:53 -0500

    add route post new roles

--- routes/apiRoutes/roleRoutes.js
+++ routes/apiRoutes/roleRoutes.js
@@ -13,7 +13,7 @@ router.get('/roles', (req, res) => {
             res.status(500).json({ error: err.message });
             return;
         }
-        res.json ({
+        res.json({
             message: 'success',
             data: rows
         });
@@ -21,6 +21,29 @@ router.get('/roles', (req, res) => {
 });
 // create a get route to view the total utilized budget of a department (combined salaries of all employees in that department)
 // create a post route to add
+router.post('/role', ({ body }, res) => {
+    const errors = inputCheck(body, 'title', 'salary', 'department_id');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+
+    const sql = `INSERT INTO roles (title, salary, department_id)
+    VALUES (?,?,?)`;
+    const params = [body.title, body.salary, body.department_id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
+
 // create a delete route
 // export router
 module.exports = router;
