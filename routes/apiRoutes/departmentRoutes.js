76703ae2214d7bee180006bc1ee1d85f1a2a3597
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-07-18 09:30:13 -0500

    add select all and delete routes for departments

--- routes/apiRoutes/departmentRoutes.js
+++ routes/apiRoutes/departmentRoutes.js
@@ -1,6 +1,48 @@
 // import necessary files
+const express = require('express');
+const router = express.Router();
+const db = require('../../db/connection');
+const inputCheck = require('../../utils/inputCheck');
+
 // create a get route to select all
-// create a get route to view the total utilized budget of a department (combined salaries of all employees in that department)
-// create a post route to add
+router.get('/departments', (req, res) => {
+    const sql = `SELECT * FROM departments`;
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
 // create a delete route
+router.delete('/department/:id', (req, res) => {
+    const sql = `DELETE FROM departments WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.statusMessage(400).json({ error: res.message});
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Department not found'
+            });
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
+
+// create a post route to add
+
+
 // export router
+module.exports = router;
